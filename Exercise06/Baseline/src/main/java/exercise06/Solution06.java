020f6f05c83f74552c1f0eaf69a430d3e196b035
Perry-codes <89555259+Perry-codes@users.noreply.github.com>
2021-09-08 22:08:31 -0400

    initial Ex06 commit

---
+++ Exercise06/Baseline/src/main/java/exercise06/Solution06.java
@@ -0,0 +1,27 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 2 Solution
+ * Copyright 2021 Maggie Perry
+ */
+/*
+Retirement Calculator
+set to accept user input
+prompt user for current age
+save current age to int currentAge
+prompt user for retirement age
+save retirement age to int retireAge
+push ages to calculator
+calculate years till retirement
+    retireAge - currentAge
+calculate year of retirement
+    get current year from system time
+
+ */
+
+package exercise06;
+
+public class Solution06 {
+
+    public static void main(String[] args) {
+
+    }
+}
